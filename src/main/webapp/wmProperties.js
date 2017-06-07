54982394c5a04cf7d901669c06dc7f1a7e00fa5e
root <root@210995f32a0c>
2017-06-07 21:40:09 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "G5Sample",
+  "homePage" : "Main",
+  "name" : "G5Sample",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
